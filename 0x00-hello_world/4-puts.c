dda0adfdb18f1bf0d67b5d002e913f7aa85461ff
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-09-08 10:33:11 -0700

    Create a prograe that prints to stdout using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints out the strings
+ * passed to puts() to the standard output
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
