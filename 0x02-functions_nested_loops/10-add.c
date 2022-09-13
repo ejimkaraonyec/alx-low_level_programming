63dcce5623b30d8bb4f02fab52faa3d52a1a3277
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-09-13 14:05:20 -0700

    compute sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * add - add integers
+ *
+ * @a: firdt integer
+ * @b: second integer
+ * Description: compute the sum of two integers
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
