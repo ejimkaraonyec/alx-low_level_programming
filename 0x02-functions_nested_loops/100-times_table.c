cb37ecf971b990e97d0c43359b095756eadfc51c
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-09-13 18:05:54 -0700

    Compute multiplication table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - print times table
+ *
+ * @n: integer parameter
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int hor, vet, prod;
+
+	if (n < 15 || n > 0)
+	{
+		for (hor = 0; hor <= n; hor++)
+		{
+			for (vet = 0; vet <= n; vet++)
+			{
+				if (vet == 0)
+					_putchar(48);
+				else if (prod < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_purchar(' ');
+					_putchar(prod % 10 + 48);
+				}
+				else if (prod >= 10 && prod < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((prod / 10) % 10 + 48);
+					_putchar(prod % 10 + 48);
+				}
+				else if
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(prod / 100 + 48);
+					_putchar((prod / 10) % 10 + 48);
+					_putchar(prod % 10 + 48);
+				}
+			}
+			_putchar(10);
+		}
+	}
+}
