dbbcb1a5931b8884af9774ae67f10c529e024316
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-09-13 18:31:39 -0700

    Compute multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,7 +37,7 @@ void print_times_table(int n)
 					_putchar((prod / 10) % 10 + 48);
 					_putchar(prod % 10 + 48);
 				}
-				else if (prod > 99 && prod < 100)
+				else if (prod > 99 && prod < 1000)
 				{
 					_putchar(',');
 					_putchar(' ');
