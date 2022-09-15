0b3486284eafecbc4cfa9165650761ff2fec4e5a
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-09-15 11:51:42 -0700

    Print some

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints from 0 to 9
+ *
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int min, max;
+
+	min = 48;
+	max = 57;
+	while (min <= max)
+	{
+		if (min != 2 || min != 4)
+			_putchar(min);
+		min++;
+	}
+	_putchar(10);
+}
