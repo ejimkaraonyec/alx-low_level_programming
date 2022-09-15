329e2fb889c98492571fee12423f69020191fed0
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-09-15 11:41:10 -0700

    Print from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 to 9
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int min, max;
+
+	min = 48;
+	max = 57;
+	while (min <= max)
+		_putchar(min);
+}
