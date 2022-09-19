731c655c49d2067bb47230bc601081435d4bb5e6
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-09-19 10:36:30 -0700

    Overwrite a variable using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - function to reset a variable using a pointer
+ *
+ * @a: pointer variable points to an int
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
