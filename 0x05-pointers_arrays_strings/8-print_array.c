2f04aa91ce04c1f2eed37d3b66a9b9e8064f0657
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-09-19 14:28:55 -0700

    Print elements in an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,8 +14,13 @@ void print_array(int *a, int n)
 {
 	int i, last;
 
-	last = n - 1;
-	for (i = 0; i < last; i++)
-		printf("%d, ", *(a + i));
-	printf("%d\n", *(a + i));
+	if (n == 1)
+		printf("%d\n", *(a + 0));
+	else
+	{
+		last = n - 1;
+		for (i = 0; i < last; i++)
+			printf("%d, ", *(a + i));
+		printf("%d\n", *(a + i));
+	}
 }
