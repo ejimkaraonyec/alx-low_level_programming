af4fa307f52149dc33ff32da81042efc751ad18f
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-09-19 10:51:03 -0700

    swap two variables using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap two integers using pointers
+ *
+ * @a: pointer variable that points to the first int
+ * @b: pointer variable that points to the second int
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *tmp = NULL;
+
+	*tmp = *a;
+	*a = *b;
+	*b = *tmp;
+}
