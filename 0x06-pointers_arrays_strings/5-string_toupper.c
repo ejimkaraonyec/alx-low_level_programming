9a244a0079d3c9244729f818e9bdca07dc24f044
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-09-21 15:35:36 -0700

    convert to upper hot fix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,7 +12,7 @@ char *string_toupper(char *array_s)
 {
 	int i;
 
-	for (i = 0; arrayy_s[i] != '\0'; i++)
+	for (i = 0; array_s[i] != '\0'; i++)
 	{
 		if (array_s[i] >= 'a' && array_s[i] <= 'z')
 			array_s[i] = array_s[i] - 32;
