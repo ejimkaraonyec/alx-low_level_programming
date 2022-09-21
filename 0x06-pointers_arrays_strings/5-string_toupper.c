b37620fc86b0372e1d6e68bd541241cf8e51965c
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-09-21 13:43:07 -0700

    convert to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes all lowercase to uppercase
+ *
+ * @array_s: pointer
+ *
+ * Return: array of character strings
+ */
+
+char *string_toupper(char *array_s)
+{
+	int i;
+
+	for (i = 0; arrayy_s[i] != '\0'; i++)
+	{
+		if (array_s[i] >= 'a' && array_s[i] <= 'z')
+			array_s[i] = array_s[i] - 32;
+	}
+
+	return (array_s);
+}
