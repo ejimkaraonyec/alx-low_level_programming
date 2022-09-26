fed76da14ebdf67a0afb7eb4d0ee73e0ba0374e9
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-09-26 09:09:12 -0700

    gets length of a substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ *
+ * @s: pointer to the string scanned
+ * @accept: list of characters to match
+ *
+ * Return: The number of bytes in s which
+ *         consist only of bytes from accept.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int i;
+
+	for (; *s; s++)
+	{
+		for (; *accept; accept++)
+		{
+			if (*s == *accept)
+			{
+				bytes++;
+				break;
+			}
+
+			else if (*(accept + 1) == '\0')
+				return (bytes);
+		}
+	}
+
+	return (bytes);
+}
