dde2f57a9376c40267fdd95f18cec79f45463bf7
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-09-26 10:02:38 -0700

    gets length of a substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,17 +12,18 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int bytes = 0;
+	unsigned int prev, bytes = 0;
 	int i, j;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
+		prev = bytes;
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 				bytes++;
 		}
-		if (*(accept + i) == '\0')
+		if (prev == bytes))
 			break;
 	}
 
