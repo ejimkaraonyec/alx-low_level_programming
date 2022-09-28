cbb66f3815b0bb2ab85d5464cb98902544f0a5ce
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-09-28 07:25:58 -0700

    print in reverse recursively

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * _strlenrec - calculate length of string
- *
- * @s: string pointer
- *
- * Return: void
- */
-
-int _strlenrec(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlenrec(s + 1));
-}
-
-/**
  * _print_rev_recursion - prints a string in reverse
  *
  * @s: string pointer
@@ -25,11 +10,9 @@ int _strlenrec(char *s)
 
 void _print_rev_recursion(char *s)
 {
-	int len;
-
-	len = _strlenrec(s);
-	if (len == 1)
-		_putchar(*s);
-	_putchar(*(s + len));
-	_print_rev_recursion(s - 1);
+	if (*s != '\0')
+	{
+		_print_rev_recursion((s + 1));
+		_putchar(*(s + 0));
+	}
 }
