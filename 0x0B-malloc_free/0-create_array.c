7eec097bcf2757f7d0e701257f538ccd48203b2b
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-10-04 05:01:54 -0700

    Creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	arr = (char*) malloc(sizeof(char) * size);
+	arr = (char *) malloc(sizeof(char) * size);
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
