11fea3b27a64d317d6904ea87bf266c38d7d6fd9
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-10-04 07:09:23 -0700

    splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -22,7 +22,7 @@ char **strtow(char *str)
 	words = count_words(str);
 	if (words == 0)
 		return (NULL);
-	strings = (char *) malloc(sizeof(char *) * (words + 1));
+	strings = malloc(sizeof(char *) * (words + 1));
 	if (strings == NULL)
 		return (NULL);
 	for (w = 0; w < words; w++)
