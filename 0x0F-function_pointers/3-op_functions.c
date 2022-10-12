17da98b72de6e76bec2e4389da3440e2e91e13b7
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-10-12 15:48:54 -0700

    simple calcs

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * op_add - sums two numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calculates
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calculates product
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calculates quotient
+ * @a: dividend
+ * @b: divisor
+ *
+ * Return: quotient
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates modulo
+ * @a: dividend
+ * @b: divisor
+ *
+ * Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
