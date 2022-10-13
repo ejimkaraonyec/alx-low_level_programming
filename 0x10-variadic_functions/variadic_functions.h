f5e48a98e4d234000e565f5d1e02912f029b5ce1
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-10-13 15:05:50 -0700

    update header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,4 +8,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct op - struct opperators
+ * @c: char type
+ * @f: associated function
+ */
+
+typedef struct opperator
+{
+	char c;
+	void (*f)(va_list);
+} op_t;
+
 #endif
