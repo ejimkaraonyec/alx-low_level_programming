396b10b6d976cf8a00899c3fefe25183ad8986b6
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-10-20 15:46:07 -0700

    writes in assembly and prints to stdout

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+section .data
+message:	db "Hello, Holberton", 0
+format:	db "%s", 10, 0
+
+	section .text
+	extern printf
+	global main
+main:
+	mov esi, message
+	mov edi, format
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
