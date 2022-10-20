f604121e82f102c55988ab02eb0447aa50450b09
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-10-20 13:45:00 -0700

    adds a new node at the beginning of a list_t list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,7 +20,7 @@ list_t *add_node(list_t **head, const char *str)
 	new->str = strdup(str);
 
 	for (lenght = 0; *str != '\0'; str++)
-		lenght;
+		lenght++;
 
 	new->len = lenght;
 	new->next = *head;
