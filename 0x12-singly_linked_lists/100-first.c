6e2b5e4f9bdfecf11f6734f5f9fa0b7983d085b3
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-10-20 16:01:24 -0700

    update function declaration sequence

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
-void before(void) __attribute__((constructor));
+void __attribute__((constructor)) before(void);
 
 /**
  * before - print before the main function
  * Return: void
  */
 
-void before(void) __attribute__((constructor))
+void __attribute__((constructor)) before(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 	       "I bore my house upon my back!\n");
