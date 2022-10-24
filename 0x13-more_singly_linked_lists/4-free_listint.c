06fbd008f303eae8a307e97f0f64c11de23283a1
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-10-24 07:06:35 -0700

    frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: head of linked list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	temp = head;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
+}
