039cab2e0cb2ccb8f68596f2fdaae159b246b31e
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-10-31 10:53:33 -0700

    update the code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,14 +18,14 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (reader == -1)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
-
-	for (; text_content[count] != '\0'; count++)
-		;
-	output = write(reader, text_content, count);
-	if (output == -1)
-		return (-1);
+	if (text_content)
+	{
+		for (; text_content[count] != '\0'; count++)
+			;
+		output = write(reader, text_content, count);
+		if (output == -1)
+			return (-1);
+	}
 	close(reader);
 	return (1);
 }
