21254c5aa678d8fbdca1a97b3e1d2871031a1ccf
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-10-31 10:35:12 -0700

    reads a text file and prints to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "mainh"
+
+/**
+ * read_textfile - reads a text file and prints it
+ * to the POSIX standard output
+ * @filename: ptr to the file to read and print
+ * @letters: number of bytes to print
+ *
+ * Return: number of bytes printed; else, 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters + 1);
+	if (buffer == NULL)
+		return (0);
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+		return (0);
+
+	buffer[letters + 1] = '\0';
+	close(fd);
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1)
+		return (0);
+
+	free(buffer);
+
+	return (bytes_read);
+}
